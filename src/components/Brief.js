97c9d092beafaf981d9f46acae18404f52e48f37
Ansh <shri.ansh4563@gmail.com>
2021-02-20 23:37:50 +0530

    UI Completed

--- src/components/Brief.js
+++ src/components/Brief.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Avatar, Button, FlexboxGrid, Icon, Modal, Panel } from 'rsuite'
+import { Avatar, Divider, FlexboxGrid, Icon, Modal, Panel } from 'rsuite'
 
 const Brief = ({ character }) => {
   const [show, setShow] = useState(false)
@@ -28,9 +28,45 @@ const Brief = ({ character }) => {
       </Panel>
       <Modal size='xs' show={show} onHide={close}>
         <Modal.Header></Modal.Header>
-        <Modal.Body id='modal-body'>
-          <Avatar circle src={character.image} />
-          {/* <img src={character.image} alt={character.name} /> */}
+        <Modal.Body>
+          <FlexboxGrid justify='space-around' align='top'>
+            <FlexboxGrid.Item colspan={8}>
+              <Avatar circle src={character.image} size='lg' />
+            </FlexboxGrid.Item>
+            <FlexboxGrid.Item colspan={12} style={{ marginTop: '1rem' }}>
+              <p className='title'>{character.name}</p>
+              <p>
+                <Icon
+                  icon='circle'
+                  style={{ color: '#b0c8d1', marginRight: '5px' }}
+                />
+                {character.status} - {character.species}
+              </p>
+            </FlexboxGrid.Item>
+          </FlexboxGrid>
+          <Divider />
+          <FlexboxGrid justify='center' align='center'>
+            <FlexboxGrid.Item colspan={8}>
+              <small>Gender</small>
+              <p>{character.gender}</p>
+            </FlexboxGrid.Item>
+            <FlexboxGrid.Item colspan={8}>
+              <small>Location</small>
+              <p>{character.location.name}</p>
+            </FlexboxGrid.Item>
+            <FlexboxGrid.Item
+              colspan={24}
+              style={{ margin: '0.8rem 0' }}
+            ></FlexboxGrid.Item>
+            <FlexboxGrid.Item colspan={8}>
+              <small>Species</small>
+              <p>{character.species}</p>
+            </FlexboxGrid.Item>
+            <FlexboxGrid.Item colspan={8}>
+              <small>Origin</small>
+              <p>{character.origin.name}</p>
+            </FlexboxGrid.Item>
+          </FlexboxGrid>
         </Modal.Body>
       </Modal>
     </>
